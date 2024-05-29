f1df898aed5f9ebc9f34fa50e477f3cb5e60b77d
ninjaguardian <cjfairchild51@gmail.com>
2024-05-29 18:24:00 -0500

    TEST_BLOCK_FALl -> TEST_BLOCK_FALL

--- src/main/java/net/obf/crystallized/sound/ModSounds.java
+++ src/main/java/net/obf/crystallized/sound/ModSounds.java
@@ -19,14 +19,14 @@ public class ModSounds {
 
     public static final RegistryObject<SoundEvent> TEST_BLOCK_BREAK = registerSoundEvents("test_block_break");
     public static final RegistryObject<SoundEvent> TEST_BLOCK_STEP = registerSoundEvents("test_block_step");
-    public static final RegistryObject<SoundEvent> TEST_BLOCK_FALl = registerSoundEvents("test_block_fall");
+    public static final RegistryObject<SoundEvent> TEST_BLOCK_FALL = registerSoundEvents("test_block_fall");
     public static final RegistryObject<SoundEvent> TEST_BLOCK_PLACE = registerSoundEvents("test_block_place");
     public static final RegistryObject<SoundEvent> TEST_BLOCK_HIT = registerSoundEvents("test_block_hit");
 
 
 
     public static final ForgeSoundType TEST_BLOCK_SOUNDS = new ForgeSoundType(1f,1f,
-            ModSounds.TEST_BLOCK_BREAK,ModSounds.TEST_BLOCK_STEP,ModSounds.TEST_BLOCK_PLACE,ModSounds.TEST_BLOCK_HIT,ModSounds.TEST_BLOCK_FALl);
+            ModSounds.TEST_BLOCK_BREAK,ModSounds.TEST_BLOCK_STEP,ModSounds.TEST_BLOCK_PLACE,ModSounds.TEST_BLOCK_HIT,ModSounds.TEST_BLOCK_FALL);
     private static RegistryObject<SoundEvent> registerSoundEvents(String name) {
         return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Crystallized.MODID, name)));
     }
